76fe2d1386dcc61308742a314c7104b0d9871b0b
Ishimwe Rwigamba Kenny Louange <kennyrwigamba@gmail.com>
2026-06-24 20:51:24 +0200

    solved the issues on 0_gather

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -10,28 +10,28 @@ API_URL = "https://jsonplaceholder.typicode.com"
 
 
 def main():
-	"""Print the completed tasks for the employee identified by argv."""
-	employee_id = int(argv[1])
+    """Print the completed tasks for the employee identified by argv."""
+    employee_id = int(argv[1])
 
-	with urlopen(f"{API_URL}/users/{employee_id}") as response:
-		employee = json.load(response)
+    with urlopen(f"{API_URL}/users/{employee_id}") as response:
+        employee = json.load(response)
 
-	with urlopen(f"{API_URL}/todos?userId={employee_id}") as response:
-		tasks = json.load(response)
+    with urlopen(f"{API_URL}/todos?userId={employee_id}") as response:
+        tasks = json.load(response)
 
-	completed_tasks = [task for task in tasks if task.get("completed")]
+    completed_tasks = [task for task in tasks if task.get("completed")]
 
-	print(
-		"Employee {} is done with tasks({}/{}):".format(
-			employee.get("name"),
-			len(completed_tasks),
-			len(tasks),
-		)
-	)
+    print(
+        "Employee {} is done with tasks({}/{}):".format(
+            employee.get("name"),
+            len(completed_tasks),
+            len(tasks),
+        )
+    )
 
-	for task in completed_tasks:
-		print("\t {}".format(task.get("title")))
+    for task in completed_tasks:
+        print("\t {}".format(task.get("title")))
 
 
 if __name__ == "__main__":
-	main()
+    main()
